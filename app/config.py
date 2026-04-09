75d60d0cc48f8816b73611ca27eaf6bc3d6b25c7
ivancr12 <ivancesped2005@gmail.com>
2026-04-08 18:51:07 -0600

    Configuración completa con S3 funcionando

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-    AWS_REGION = os.getenv('AWS_REGION', 'us-east-1')
-    S3_BUCKET = os.getenv('S3_BUCKET', 'tp7-mlops-bucket')
+    AWS_REGION = os.getenv('AWS_REGION', 'us-east-2')
+    S3_BUCKET = os.getenv('S3_BUCKET', 'mlops-tp7-cespedes-2026')
     MODEL_PATH = 'models/model.pkl'
     DATA_PATH = 'data/housing_data.csv'
